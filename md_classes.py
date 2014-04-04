c25916c91d6462bda4b3aba9a8dd2b80ab62d81d
Pablo Alcain <pabloalcain@gmail.com>
2014-04-04 06:01:27 -0300

    Slight changes to md_classes, doc improved
    Some name refactoring is needed!

--- md_classes.py
+++ md_classes.py
@@ -28,8 +28,21 @@ class mdsys(object):
 
   def build_script(self, fname = "lammps.inp"):
     """
-    We get all the information from this object and build the input
-    file. As a file so we can properly debug if needed. We can also
+    We get all the information from this object and build an input
+    file. This input file does the following:
+    
+    - insert masses and number of particles
+    - create velocities from a Gaussian at the set temperature
+    - set the interaction according to the table
+    - minimize system to remove the potential energy
+    - set the fix nvt at the required temperature
+    - set thermo style and frequency (can be overridden later?)
+    - set dump style
+
+    After this, to make a proper run, the dump frequency should be 
+    manually set *outside* the molecular dynamics system.
+    
+    As a file so we can properly debug if needed. We can also
     check if some kwargs should be added to specify when we want a
     lattice or a given data/restart/dump file, but since this seems
     outdated and hard to extend, it is not implemented yet.
@@ -63,12 +76,14 @@ min_style	hftn
 minimize	0 1.0 1000 1000000
 
 pair_coeff	1 1 {table_fname} PP {cutoff}
+fix		1 all nvt temp {T} {T} {tdamp}
 """.format(size=(self.N/self.d)**(1.0/3.0),
 	             nprot=self.x * self.N,
 		     nneut=(1-self.x) * self.N,
 		     T=self.T,
 		     table_fname=self.table_fname,
 		     cutoff=max(5.4,self.l),
+		     tdamp=10.0,
 		     seed1=R.randint(0,10000),
 		     seed2=R.randint(0,10000),
 		     seed3=R.randint(0,10000),
