97dbac1255df52b809a16e041d8feda2f56aa701
Pablo Alcain <pabloalcain@gmail.com>
2014-04-04 05:07:33 -0300

    Input file method begun.
    One step closer, and lacks some functionality (like choosing input
    file)

--- md_classes.py
+++ md_classes.py
@@ -1,12 +1,6 @@
-class mdsys(Object):
-  self.T = 1.0
-  self.l = 20
-  self.N = 5000
-  self.x = 0.5
-  self.d = 0.04
-  self.V = "med"
-  self.filename = "table.dat"
+import random as R
 
+class mdsys(object):
   def __init__(self, T, l, N, x, d, V):
     """
     Constructor: so far we need to pass all the variables. Eventually
@@ -37,14 +31,50 @@ class mdsys(Object):
     We get all the information from this object and build the input
     file. As a file so we can properly debug if needed. We can also
     check if some kwargs should be added to specify when we want a
-    lattice, but since this seems outdated and hard to extend, it is
-    not implemented yet.
+    lattice or a given data/restart/dump file, but since this seems
+    outdated and hard to extend, it is not implemented yet.
     """
     self.input_fname = fname
-    """ BUNCH OF DIRTY FUNCTIONS """
-    pass
-  
+    inp = """#Nuclear model
+units		lj
+atom_style	atomic
+timestep	0.10
 
+region		box block 0 {size} 0 {size} 0 {size}
+create_box	2 box
+create_atoms	1 random {nprot} {seed1} box
+create_atoms	2 random {nneut} {seed2} box
+mass		1 938.0
+mass		2 938.0
+velocity	all create {T} {seed3}
+
+pair_style	table linear 
+pair_coeff	1 1 {table_fname} NN 5.4
+pair_coeff	1 2 {table_fname} NP 5.4
+pair_coeff	2 2 {table_fname} NN 5.4
+
+neighbor	1.2 bin
+neigh_modify	every 1 delay 0 check yes one 8000 page 80000
+
+thermo_style	custom step temp epair etotal press
+thermo		1000
+
+min_style	hftn
+minimize	0 1.0 1000 1000000
+
+pair_coeff	1 1 {table_fname} PP {cutoff}
+""".format(size=(self.N/self.d)**(1.0/3.0),
+	             nprot=self.x * self.N,
+		     nneut=(1-self.x) * self.N,
+		     T=self.T,
+		     table_fname=self.table_fname,
+		     cutoff=max(5.4,self.l),
+		     seed1=R.randint(0,10000),
+		     seed2=R.randint(0,10000),
+		     seed3=R.randint(0,10000),
+		     )
+    with open(self.input_fname, 'w') as fp:
+      print>>fp, inp
 
   def set_T(self, T):
     self.T = T
