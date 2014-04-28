61f2ecf1fb86a4bf47d2ba9897e9f2203836113a
pablo <pablo@lucy.df.uba.ar>
2014-04-27 22:03:52 -0300

    Added gpu file

---
+++ src/gpu.py
@@ -0,0 +1,25 @@
+"""
+This file is an example of how the MDSys class should be imported and
+used. The main idea is that everything can be called like this, and
+therefore looped from within Python
+"""
+from md_classes import MDSys
+from lammps import lammps
+from numpy import linspace
+
+temp=linspace(1.6,0.1,60)
+T = 1.7
+l = 20
+N = 5000
+x = 0.5
+d = 0.05
+V = "medium"
+system = MDSys(T, l, N, x, d, V, gpu=True)
+system.build_script(fname="lammps.inp")
+system.setup()
+for i in temp:
+  system.run(50000)
+  system.set_T(i) 
+#  system.results()
+# RUN Nsteps steps
+# POSTPROCESS
