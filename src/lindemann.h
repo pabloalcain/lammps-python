0fa38c5152328efeb2b66b832bfa0efba88f270b
Pablo Alcain <pabloalcain@gmail.com>
2014-05-17 02:54:04 -0300

    Cleaned analysis directory

--- src/lindemann.h
+++
@@ -1,7 +0,0 @@
-#include "lammps.h"         // these are LAMMPS include files
-#include "input.h"
-#include "atom.h"
-#include "library.h"
-
-using namespace LAMMPS_NS;
-double lindemann(LAMMPS *lmp);
