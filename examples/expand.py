cece96b89a02737b1b2c47860164385c735989f1
Pablo Alcain <pabloalcain@gmail.com>
2015-04-04 14:41:34 -0300

    Added expand script

---
+++ examples/expand.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+"""
+This file is an example of how the MDSys class should be imported and
+used. The main idea is that everything can be called like this, and
+therefore looped from within Python
+"""
+from neutronstar.MDSys import MDSys
+from lammps import lammps
+import numpy as np
+from itertools import product
+from sys import stdout
+
+temp = (2.0,)#np.linspace(2.0,0.5,31)
+param = {}
+param['T'] = 2.0
+param['lambda'] = 20
+param['N'] = 60
+param['x'] = 0.4
+darr = np.linspace(0.01,0.08,8)
+param['potential'] = 'medium'
+comp = ('mste', 'thermo')
+for d in darr:
+    param['d'] = d
+    print "x={0} d={1}".format(0.4, d)
+    system = MDSys(silent=False)
+    system.setup(param, comp)
+    system.minimize()
+    for T in temp:
+        param['T'] = T
+        system.setup(param, comp)
+        print "T: {0:>6}. Equilibrating...".format(T),
+        stdout.flush()    
+        #system.equilibrate(wind = 20, nfreq = 1000)
+        system.expand(0.01)
+        print "\bDone! Evolution.....0%",
+        stdout.flush()
+        for j in range(50):
+            system.run(1000)
+            system.results()
+            system.dump()
+            print '\b\b\b\b\b{0:.>3}%'.format((j+1)*2),
+            stdout.flush()
+        system.unexpand()
+        system.flush()
+        print '\b\b\b\b\bDone!\r'
