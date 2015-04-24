7062a558897897bea5970b5f8b803350c7905eca
Pablo Alcain <pabloalcain@gmail.com>
2015-04-24 04:41:40 +0200

    Fixed potential, added Horowitz

--- neutronstar/MDSys.py
+++ neutronstar/MDSys.py
@@ -20,10 +20,7 @@ def build_table(pot, l):
 
   - Pandha medium
   - Pandha stiff
-  - Horowitz (not yet implemented)
-
-  The code here wasn't given much thought so maybe it can be improved
-  by whoever wants to give it a try, and, at least, make it cleaner!
+  - Horowitz
   """
 
   rc_nuc = 5.4
@@ -55,37 +52,53 @@ def build_table(pot, l):
       (Vr, Va, V0) = (3601.482, 2834.338, 17630.256)
       (ur, ua, u0) = (2.2395, 2.0, 3.25)
 
+    def Vnn(r):
+      return V0 * np.exp(-u0 * r) / r
+    
+    def Vnp(r):
+      return Vr * np.exp(-ur * r) / r - Va * np.exp(-ua * r) / r
+      
+    def Vpp(r):
+      return V0 * np.exp(-u0 * r) / r + Vc * np.exp(-uc * r) / r
 
-    descr['NN'] = "# Pandha {0} potential for same species".format(pot)
-    V['NN'] = V0 * np.exp(-u0 * r['NN']) / r['NN']
-    F['NN'] = V0 * np.exp(-u0 * r['NN']) / (r['NN'])**2 * (u0 * r['NN'] + 1)
-
-    descr['NP'] = "# Pandha {0} potential for different species".format(pot)
-    V['NP'] = Vr * np.exp(-ur * r['NP']) / r['NP'] -\
-              Va * np.exp(-ua * r['NP']) / r['NP']
-    F['NP'] = Vr * np.exp(-ur * r['NP']) / (r['NP'])**2 * (ur * r['NP'] + 1) -\
-              Va * np.exp(-ua * r['NP']) / (r['NP'])**2 * (ua * r['NP'] + 1)
-
-    descr['PP'] = "# Pandha {0} potential for same species \
-    with Coulomb interaction lambda = {1}".format(pot, l)
-    V['PP'] = V0 * np.exp(-u0 * r['PP']) / r['PP'] +\
-              Vc * np.exp(-uc * r['PP']) / r['PP']
-    F['PP'] = V0 * np.exp(-u0 * r['PP']) / (r['PP'])**2 * (u0 * r['PP'] + 1) +\
-              Vc * np.exp(-uc * r['PP']) / (r['PP'])**2 * (uc * r['PP'] + 1)
 
   elif pot == "horowitz":
-    raise AttributeError("Horowitz potential not yet implemented! Sorry :(")
+    a = 110.0
+    b = -26.0
+    c = 24.0
+    L = 1.25
+    def Vnn(r):
+      return a * np.exp(-r**2 / L) + (b - c) * np.exp(-r**2 / (2*L))
+
+    def Vnp(r):
+      return a * np.exp(-r**2 / L) + (b + c) * np.exp(-r**2 / (2*L))
+
+    def Vnn(r):
+      return a * np.exp(-r**2 / L) + (b - c) * np.exp(-r**2 / (2*L)) + Vc * np.exp(-uc * r) / r
 
   else:
     raise AttributeError("Option {0} for potential not found".format(pot))
+
+  V['NN'] = Vnn(r['NN'])
+  V['NP'] = Vnp(r['NP'])
+  V['PP'] = Vpp(r['PP'])
+
+  descr['NN'] = "# {0} potential for same species".format(pot)
+  descr['NP'] = "# {0} potential for different species".format(pot)
+  descr['PP'] = ("# {0} potential for same species "
+                 "with Coulomb interaction lambda = {1}").format(pot, l)
+
 
   with open("potential.table", 'w') as fp:
     for p in pairs:
       print>>fp, descr[p]+"\n"
       print>>fp, p
       print>>fp, "N {0}\n".format(N)
-      for i in xrange(N):
-        print>>fp, i+1, r[p][i], V[p][i], F[p][i]
+      _r = r[p]
+      _V = V[p]
+      _F = - np.diff(_V) / np.diff(_r)
+      for i in xrange(N-1): # improve with enumerate
+        print>>fp, i+1, _r[i], _V[i], _F[i]
 
 
 
