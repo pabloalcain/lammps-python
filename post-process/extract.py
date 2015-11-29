83791da666d8221145a58ca3d69a9b76238c8ebd
Pablo Alcain <pabloalcain@gmail.com>
2015-11-29 05:07:05 -0300

    Rewritten post-process, now looks nice

--- post-process/extract.py
+++ post-process/extract.py
@@ -1,56 +1,168 @@
 """
-This file has routines that extract information from dump lammps files
+Module for post-process in neutronstars
 """
 import numpy as np
+from collections import OrderedDict
+import os
 
-def positions(fname, step):
-  """
-  Extract positions at a definite timestep from a lammps file
-  """
-  f = open(fname)
-  n = 0
-  for line in f.readlines():
-    n += 1
-    if n < 4:
-      continue
-    if n == 4:
-      npart = int(line)
-      s1 = 9 * (step + 1) +  step * npart + 1
-      x = np.zeros((npart, 3))
-      continue
-    if n < s1:
-      continue
-    idxp = n - s1
-    for j in range(3): x[idxp, j] = line.split()[j + 2]
-    if n == s1 + npart -1:
-      break
-  f.close()
-  return x
+PREFIX = OrderedDict()
+PREFIX['V'] = ''
+PREFIX['l'] = 'l'
+PREFIX['x'] = 'x'
+PREFIX['N'] = 'N'
+PREFIX['d'] = 'd'
+PREFIX['T'] = 'T'
 
-def types(fname, step):
+COLS = {'x': (2, 3, 4), 'v': (5, 6, 7), 'clus': (8,), 'type': (1,)}
+DATA_TYPE = {'x': np.double, 'v': np.double, 'clus': np.int32, 'type': np.int32}
+
+def _check(path):
+  """Check whether path has only one possible value. If so, we don't
+  need to set it from the outside and returns the only choice
+
   """
-  Extract types at a definite timestep from a lammps file
+
+  if len(os.listdir(path)) > 1:
+    helper = "Multiple options for directory {0}: {1}. Please specify a value"
+    raise AttributeError(helper.format(path, os.listdir(path)))
+  return os.listdir(path)[0]
+
+class Extraction(object):
   """
-  f = open(fname)
-  n = 0
-  for line in f.readlines():
-    n += 1
-    if n < 4:
-      continue
-    if n == 4:
-      npart = int(line)
-      s1 = 9 * (step + 1) +  step * npart + 1
-      t = np.zeros(npart, dtype=np.int32)
-      continue
-    if n < s1:
-      continue
-    idxp = n - s1
-    t[idxp] = line.split()[1]
-    if n == s1 + npart -1:
-      break
-  f.close()
-  return t
+  Extraction class. We can extract either scalars, vector or
+  per-particle magnitudes
+
+  """
+  # TODO: make getter attributes and abstract the array/particle part
+  def __init__(self, path='.'):
+    self.path = path
+
+  def _set_path(self, parameters):
+    """Parameters is a dictionary with entries for the 6 parameters we
+    use when we search in a root data directory; V, l, x, N, d, T
+
+    Here we *believe* on the dir structure:
+    {V}/l{l}/x{x}/N{N}/d{d}/T{T}
+    """
+
+    _path = self.path
+    #for every possible parameter, we check whether it was passed or not
+    for _param in PREFIX:
+      if _param in parameters:
+        direct = PREFIX[_param] + str(parameters[_param])
+      else:
+        direct = _check(_path)
+      _path += '/' + direct
+    return _path
+
+  def scalar(self, mag, parameters):
+    """
+    Extract a single collective value with its stdev.
+    It doesn't support anymore height, that came from the
+    fit method. Now lambda makes the conversion from k to
+    lambda. Possible magnitudes:
+    - breadth
+    - S_absorption
+    - pressure
+    - size_avg
+    - k_absorption
+    - surface
+    - volume
+    - size_std
+    - potential
+    - kinetic
+    - energy
+    - euler
+    - temperature
+
+    - lambda (conversion from k_absoprtion)
+    """
+    _path = self._set_path(parameters)
+    try:
+      _file = open(_path + "/thermo.dat")
+    except IOError:
+      return 0, 0
+    header = _file.readline()[2:-1]
+    _file.close()
+
+    if mag == 'lambda':
+      idx = header.split(', ').index('k_absorption')
+    else:
+      idx = header.split(', ').index(mag)
+    therm = np.loadtxt(_path +'/thermo.dat')
+    avg = np.mean(therm, axis=0)[idx]
+    std = np.std(therm, axis=0)[idx]
+    if mag == 'lambda':
+      std = 2*np.pi/(avg*avg) * std
+      avg = 2*np.pi/avg
+    return avg, std
+
+  def array(self, mag, parameters):
+    """
+    Extract an array magnitude, that is in a textfile inside the
+    directory. Possible magnitudes are:
+
+    - cluster
+    - rdf
+    - ssf
+
+    Obviously each one has a different structure, so we only pass the
+    parsed array as a matrix
+    """
+    _path = self._set_path(parameters)
+    try:
+      out = np.loadtxt('{0}/{1}.dat'.format(_path, mag))
+    except ValueError:
+      out = np.loadtxt('{0}/{1}.dat'.format(_path, mag), delimiter=',')
+    return out
+
+  def particle(self, mag, parameters):
+    """
+    Extract a 'per particle' magnitude that is in a lammps dump file
+    inside the directory. Get only the first timestep. Possible
+    magnitudes are:
+
+    - x
+    - v
+    - type
+    - clus
+    """
+    _path = self._set_path(parameters)
+    _size = len(COLS[mag])
+    _file = open('{0}/dump.lammpstrj'.format(_path))
+    _nline = 0
+    for line in _file.readlines():
+      _nline += 1
+      if _nline < 4:
+        continue
+      if _nline == 4:
+        npart = int(line)
+        out = np.zeros((npart, _size), dtype=DATA_TYPE[mag])
+        continue
+      if _nline < 10:
+        continue
+      if _nline == npart + 10:
+        break
+      idxp = _nline - 10
+      for i, j in enumerate(COLS[mag]):
+        out[idxp, i] = line.split()[j]
+    _file.close()
+    return out
+
+def main():
+  """Main function with a use case, we extract the energy and compare
+  between horowitz and medium eos.
+  """
+  ext = Extraction('/home/pablo/artemis/eos/data')
+  mag = 'energy'
+  parameters = {'x': 0.5, 'd': 0.08, 'T': 0.5, 'V': 'horowitz'}
+  horowitz = ext.scalar(mag, parameters)
+  parameters['V'] = 'medium'
+  medium = ext.scalar(mag, parameters)
+  print 'Energy for medium: {0} +/- {1}'.format(*medium)
+  print 'Energy for horowitz: {0} +/- {1}'.format(*horowitz)
+  typ = ext.particle('type', parameters)
+  print typ
 
 if __name__ == "__main__":
-  x = positions("dump.lammpstrj", 1)
-  t = types("dump.lammpstrj", 1)
+  main()
---
+++ post-process/plotter.py
@@ -0,0 +1,92 @@
+"""
+This automates many plots usually done when post-processing
+"""
+
+import matplotlib
+_FONT = {'size'   : 18}
+matplotlib.rc('font', **_FONT)
+_LINES = {'linewidth' : 2.0, 'markersize': 10.0}
+matplotlib.rc('lines', **_LINES)
+_LEGEND = {'numpoints': 1}
+matplotlib.rc('legend', **_LEGEND)
+import numpy as np
+import matplotlib.pyplot as plt
+from extract import Extraction
+
+FMTL = ['^-', 's-', 'D-', 'v-', 'o-',]
+LABELS = {'energy': r'Energy [MeV]',
+          'lambda': r'$\lambda$ [fm]',
+          'S_absorption': 'Absorption peak',
+          'pressure': r'Pressure [MeV/fm$^3$]',
+          'd': r'Density [fm$^{-3}$]',
+          'T': r'Temperature [MeV]',}
+
+SRC = 'source_fig/'
+DEST = 'fig/'
+
+class Plotter(object):
+  """
+  We get a path and instantiate Extraction. From then on, we plot as
+  abstractly as possible.
+
+  # TODO: Add plots for arrays (like mste, rdf, etc)
+  """
+  def __init__(self, path='.'):
+    self.extraction = Extraction(path)
+
+  def onedim(self, axis, mag, parameters, **kwargs):
+    """
+    This function plots in axis a magnitude mag as a function of
+    whichever parameter that is an interable
+    """
+    _param = parameters.copy()
+    for key, value in parameters.iteritems():
+      try:
+        len(value)
+        if not isinstance(value, str):
+          var = key
+          xval = value
+      except TypeError:
+        pass
+
+    yval = np.zeros_like(xval)
+    yerr = np.zeros_like(xval)
+    for i, x in enumerate(xval):
+      _param[var] = x
+      ext = self.extraction.scalar(mag, _param)
+      yval[i] = ext[0]
+      yerr[i] = ext[1]
+    axis.errorbar(xval, yval, yerr=yerr, **kwargs)
+    axis.set_xlabel(LABELS[var])
+    axis.set_ylabel(LABELS[mag])
+    return
+
+  def multiplot(self, run, mag, parameters):
+    """
+    From a parameters dict that has two iterables, select one as
+    'running variable' and plot for different values of it
+    """
+    _param = parameters.copy()
+    fig, axis = plt.subplots(1, 1)
+    for fmt, val in zip(FMTL, parameters[run]):
+      _param[run] = val
+      lbl = '{0} = {1}'.format(run, val)
+      self.onedim(axis, mag, _param, fmt=fmt, label=lbl)
+    axis.legend(loc='upper left')
+    fig.canvas.draw()
+    fig.tight_layout()
+    return fig, axis
+
+def main():
+  """Main function with a use case, where we plot the EoS"""
+  plot = Plotter('/home/pablo/artemis/eos/data')
+  parameters = {'x': (0.1, 0.2, 0.3, 0.4, 0.5),
+                'd': np.linspace(0.02, 0.16, 15),
+                'T': 0.5, 'V': 'medium'}
+  run = 'x'
+  mag = 'eos'
+  plot.multiplot(run, mag, parameters)
+  plt.show()
+
+if __name__ == '__main__':
+  main()
