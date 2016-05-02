cf32e3d222d12bdf7dafe9474ce1f10b2c27f5bc
Pablo Alcain <pabloalcain@gmail.com>
2016-05-02 02:34:04 -0300

    Added the refactor example

---
+++ examples/refactor.py
@@ -0,0 +1,29 @@
+"""
+Test refactor system. The way we want it to work, at least
+"""
+from neutronstar import *
+import numpy as np
+
+sys = NeutronStarSystem()
+k = np.linspace(0.2, 0.5, 200)
+computes = {'rdf': RDF(200, ((0,), (0,))),
+            'ssf' : StructureFactor(k, ((0,), (0,))),
+            'mste': MSTE()}
+analyzer = Analyzer(computes)
+
+sys['potential'] = 'medium'
+sys['lambda'] = 20
+sys['x'] = 0.5
+sys['N'] = 5000
+sys['T'] = 2.0
+sys['d'] = 0.05
+sys.minimize(0, 1.0, 1000, 100000)
+for T in np.linspace(2.0, 0.5, 31):
+  sys['T'] = T
+  log = Logger(sys)
+  sys.thermalize(200, 30)
+  analyzer.zero()
+  for i in range(50):
+    sys.run(1000)
+    log.dump(sys)
+  log.log(analyzer)
