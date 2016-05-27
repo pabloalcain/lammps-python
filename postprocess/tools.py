9ecce94513a228a48d74f1acb464ce7f5478dd81
Pablo Alcain <pabloalcain@gmail.com>
2016-05-27 20:53:32 -0300

    Added replicate tool

--- postprocess/tools.py
+++ postprocess/tools.py
@@ -1,39 +1,52 @@
 import numpy as np
 import itertools as it
-import extract
+from postprocess.extract import Extraction
 
-def replicate(fname, fout, nrep=1):
-  pos = extract.positions(fname, 0)
-  typ = extract.types(fname, 0)
-  boxorig = extract.box(fname)
-  boxnew = boxorig.copy()
 
+def replicate(x, t, box, nrep=1):
+  """
+  Replicate positions of particles in a box according to periodic
+  boundary conditions.
+
+  Parameters
+  ----------
+
+  x : numpy array
+      Positions of the particles
+
+  t : numpy array
+      Types of the particles
+
+  box : iterable
+      Simulation box
+
+  nrep : int, optional
+      Number of repetitions to consider. Default value is 1.
+
+  Returns
+  -------
+
+  x, t, box : numpy array, numpy array, box
+      Positions, type and box of the repeated values.
+  """
+  boxnew = box.copy()
   rep = np.linspace(0, 2*nrep, 2*nrep + 1) - nrep
-  for (orig, new) in zip(boxorig, boxnew):
+  for (orig, new) in zip(box, boxnew):
     new[0] -= nrep * (orig[1] - orig[0])
     new[1] += nrep * (orig[1] - orig[0])
   szorig = np.zeros(3)
-  for i, sz in enumerate(szorig):
-    szorig[i] = boxorig[i][1] - boxorig[i][0]
-  norig = np.shape(pos)[0]
+  for i, _ in enumerate(szorig):
+    szorig[i] = box[i][1] - box[i][0]
+  norig = np.shape(x)[0]
   nnew = norig * (2*nrep + 1)**3
-  f = open(fout, 'w')
-  print>>f, "ITEM: TIMESTEP"
-  print>>f, 0
-  print>>f, "ITEM: NUMBER OF ATOMS"
-  print>>f, nnew
-  print>>f, "ITEM: BOX BOUNDS pp pp pp"
-  for bx in boxnew:
-    print>>f, bx[0], bx[1]
-  print>>f, "ITEM: ATOMS id type x y z vx vy vz"
+  xnew = np.zeros((nnew, 3))
+  tnew = np.zeros((nnew, 1), dtype=np.int32)
 
-  idx = 0;
-  for n, r in enumerate(pos):
+  idx = 0
+  for n, r in enumerate(x):
     for nx, ny, nz in it.product(rep, rep, rep):
-      idx += 1
       desp = np.array((nx, ny, nz)) * szorig
-      print>>f, idx, typ[n],
-      print>>f,  ' '.join(map(str, np.array(r) + desp)), '0.0 0.0 0.0'
-
-if __name__ == "__main__":
-  replicate("test.lammpstrj", "replica.lammpstrj")
+      xnew[idx] = np.array(r) + desp
+      tnew[idx] = t[n]
+      idx += 1
+  return xnew, tnew, boxnew
