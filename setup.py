f8ba34b7406a7ed1d0fc07a15c3bc37871d505d5
Pablo Alcain <pabloalcain@gmail.com>
2016-06-03 16:38:12 -0300

    clean setup script

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
+"""
+Build and installation script
+"""
 from setuptools import setup, find_packages
 import os
-import sys
-#This is a list of files to install, and where
-#(relative to the 'root' dir, where setup.py is)
-#You could be more specific.
 
 shr_libraries = []
 for root, _, files in os.walk('.'):
@@ -12,29 +11,6 @@ for root, _, files in os.walk('.'):
       shr_libraries.append(f)
 
 packages = find_packages()
-long_description = """
-The idea of this project is to create a good framework to call
-LAMMPS from Python, mostly based on the Neutron Star typical
-simulations. The core idea is that we have a 6 dimensional parameters
-space, which are:
-
-- V: Type of potential (Medium, Stiff, Horowitz)
-
-- l: lambda, screening length of the Coulomb-Debye interaction
-
-- N: Number of particles
-
-- x: fraction of protons
-
-- d: number density of particles
-
-- T: temperature
-
-It will have *no* input file, at least in its core. Python flexibility
-allows an easy extension to support this behaviour, but the idea is to
-create python files that instantiate the classes that will be
-developed in this code."""
-
 setup(name="LAMMPS Neutron Star Wrapper",
       version="0.1",
       description="LAMMPS from Python in Neutron Star simulations",
@@ -44,6 +20,4 @@ setup(name="LAMMPS Neutron Star Wrapper",
       packages=packages,
       package_data={'' : shr_libraries},
       include_package_data=True,
-      scripts=["examples/neutronstar.py"],
-      long_description=long_description
-)
+      scripts=["examples/neutronstar.py"],)
